c9fa569ed16bbf25b3294941b0668f59a964889b
Kevin Schroeder <95885765+KevSchroeder@users.noreply.github.com>
2024-02-03 11:11:29 -0500

    adding content

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import './Navbar.css'
+import { useState } from "react";
+import './Navbar.css';
+import Logo from '../Assets/BuildingStorefront.svg'
+import { BuildingStorefrontIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 const Navbar = () => {
-    return (
-        <div>
 
+    const [menu, setMenu] = useState("shop");
+
+    return (
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={Logo} alt=""/>
+                <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={()=> {setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={()=> {setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+                <li onClick={()=> {setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
+                <li onClick={()=> {setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <ShoppingCartIcon className="h-6 w-6 text-gray-500" />
+                <div className="nav-cart-count">0</div>
+            </div>
         </div>
     )
 }
